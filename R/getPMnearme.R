4202f1640c55aa2e71a05cce5be725d1303ab223
Li <jli10@arsla40021770.ndc.nasa.gov>
2017-04-18 16:55:10 -0700

    map

--- R/getPMnearme.R
+++ R/getPMnearme.R
@@ -9,6 +9,7 @@
 #' \dontrun{getPMnearme()} #require personal token
 getPMnearme <- function(){
   dat <- getPMbyCityNames("here")
+  dat$APL <- factor(dat$APL, levels = getglobalPM25Options()$apl_description)
   mapWorld <- ggplot2::borders("world", colour="gray50", fill="gray50") # create a layer of borders
   g <- ggplot2::ggplot(ggplot2::aes(x = lon, y = lat, colour = APL, size = pm25), data = dat[complete.cases(c(dat$lat)),]) +   mapWorld
   g <- g + ggplot2::geom_point() + ggplot2::scale_color_manual(values=getglobalPM25Options()$apl_color)
